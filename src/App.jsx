58e83809be93832f7e9f202edb5475228617a81f
MarySnik <mary.snikk@gmail.com>
2025-07-30 18:46:09 +0800

    check gitlab pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import Header from './components/Header'
 import { createBrowserRouter, createRoutesFromElements, Route, Routes } from "react-router-dom"
 
-const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<h1>myy</h1>} />)
-)
+// const router = createBrowserRouter(
+  // createRoutesFromElements(<Route index element={<h1>myy</h1>} />)
+// )
 
 function App() {
 
@@ -16,8 +16,8 @@ function App() {
       <Routes>
       {/* later can create loop to create all small routes */}
 
-        <Route path="/" element={''} />
-        <Route path="/" element={''} />
+        {/* <Route path="/" element={''} /> */}
+        {/* <Route path="/" element={''} /> */}
       </Routes>
 
     </>
