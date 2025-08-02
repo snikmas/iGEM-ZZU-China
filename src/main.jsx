b654147effa865cc6203f30f477170cdc40b6c22
MarySnik <mary.snikk@gmail.com>
2025-08-02 19:26:43 +0800

    testing full path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/zzu-china'>
       <App className="font-[Arial]"/>
     </BrowserRouter>
   </StrictMode>,
--- src/utils/pathMapping.jsx
+++ src/utils/pathMapping.jsx
@@ -1,8 +1,11 @@
 import pages from "../pages";
 import React from 'react'
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+
 
 function pathMapping() {
+
+
   return (
   <Routes>
     { Object.entries(pages).map(([key, page]) => {
@@ -11,12 +14,12 @@ function pathMapping() {
 
       // no subnav
       if(more.length === 1) {
-        return (<Route key={more[0].title} to={path} element={<more.Component />} />)
+        return (<Route key={more[0].title} path={path} element={<more.Component />} />)
       }
 
       // its a subnav
       return (more.map((sub) => {
-        return (<Route key={sub.title} to={path + sub.path} element={<sub.Component/>} />)
+        return (<Route key={sub.title} path={path + sub.path} element={<sub.Component/>} />)
       })) 
     })}
   </Routes>)
