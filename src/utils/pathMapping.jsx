0f9e50a7699acb2961329b54d2a1240081776f0f
MarySnik <mary.snikk@gmail.com>
2025-08-02 19:34:19 +0800

    fixing a mess

--- src/utils/pathMapping.jsx
+++ src/utils/pathMapping.jsx
@@ -1,11 +1,8 @@
 import pages from "../pages";
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-
+import { Routes, Route} from 'react-router-dom'
 
 function pathMapping() {
-
-
   return (
   <Routes>
     { Object.entries(pages).map(([key, page]) => {
@@ -14,12 +11,12 @@ function pathMapping() {
 
       // no subnav
       if(more.length === 1) {
-        return (<Route key={more[0].title} path={path} element={<more.Component />} />)
+        return (<Route key={more[0].title} to={path} element={<more.Component />} />)
       }
 
       // its a subnav
       return (more.map((sub) => {
-        return (<Route key={sub.title} path={path + sub.path} element={<sub.Component/>} />)
+        return (<Route key={sub.title} to={path + sub.path} element={<sub.Component/>} />)
       })) 
     })}
   </Routes>)
