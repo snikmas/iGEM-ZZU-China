ccff48fba6192d4e681134714b34d94fffb805c1
MarySnik <mary.snikk@gmail.com>
2025-08-04 16:24:32 +0800

    safety: finished 2/3

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import pages from './pages'
 // import pathMapping from './utils/pathMapping'
 import { Home } from './contents'
 import { Navigate } from 'react-router-dom'
+import SideNavbar from './components/SideNavbar'
 
 function App() {
 
   return (
     // return <RouterProvider path="/home" router={router}/>
     <>
-      <Header />
+    <div className="fullSite flex flex-col gap-20">
 
-      <Routes>
+      <Header />
+      <div className='screen flex px-30 gap-30 text-base leading-relaxed'>
+        < SideNavbar />
+
+
+        <Routes>
       
         {/* i wanna redirect to home */}
         <Route path="/" element={<Navigate to="/home" replace />} />
@@ -21,39 +27,41 @@ function App() {
 
         { Object.entries(pages).map(([key, page]) => {
           const {path, more} = page;
-        
+          
           // no subnav
           if(more.length === 1 && !more[0].path) {
-
+            
             const Component = more[0].Component
             console.log(Component)
-
+            
             return (
               <Route 
-                key={more[0].title} 
-                path={path} 
-                element={< Component />} 
+              key={more[0].title} 
+              path={path} 
+              element={< Component />} 
               />)
-          }
-
-    // If there are multiple subpages (nested routing)
-          return (
-            more.map((sub) => {
-              const SubComponent = sub.Component;
-              return (
-                <Route 
+            }
+            
+            // If there are multiple subpages (nested routing)
+            return (
+              more.map((sub) => {
+                const SubComponent = sub.Component;
+                return (
+                  <Route 
                   key={path} 
                   path={`${path.replace(/^\//, '')}/${sub.path}`}
                   element={<SubComponent />}
-                />)
-      }))
-    })}  
+                  />)
+                }))
+              })}  
 
-    <Route path="*" element={<>opps, there's notihng</>}/>
-    </Routes>
+      <Route path="*" element={<>opps, there's notihng</>}/>
+        </Routes>
 
 
 
+      </div>
+      </div>
     </>
   )
 }
---
+++ src/components/SideNavbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function SideNavbar() {
+  return (
+    <div className='sideNavbar'>
+      <h4>Contents</h4>
+      <div className='sideNavbarContents'>
+        <h3>Idenfitication of Risks</h3>
+        <h3>Management of Risks</h3>
+        <h3>Responsibility in Human Practices</h3>
+      </div>
+    </div>
+  )
+}
+
+export default SideNavbar
