0cc03c756e082ec4bdfb0c61ba7081879535fdd0
snimas <mary.snikk@gmail.com>
2025-10-03 02:13:21 +0000

    Update file index.jsx

--- src/views/teammembers/index.jsx
+++ src/views/teammembers/index.jsx
@@ -5,7 +5,7 @@ import './index.css';
 // ... existing code ...
 const dongbinmentor = 'https://static.igem.wiki/teams/5822/newassets/images/team_menbers/mentors/董杉-mentor.jpg';
 const lipengmentor = 'https://static.igem.wiki/teams/5822/newassets/images/team_menbers/mentors/peili.webp';
-const liukangdongmentor = 'https://static.igem.wiki/teams/5822/newassets/images/team_menbers/mentors/刘康栋.webp';
+const liukangdongmentor = 'https://static.igem.wiki/teams/5822/newassets/images/team_menbers/mentors/kangdongliu.webp';
 const panzhifengmentor = 'https://static.igem.wiki/teams/5822/newassets/images/team_menbers/mentors/潘志峰.webp';
 const wangzhenlongmentor = 'https://static.igem.wiki/teams/5822/newassets/images/team_menbers/mentors/王振龙.jpg';
 const zangwenqiaomentor = 'https://static.igem.wiki/teams/5822/newassets/images/team_menbers/mentors/臧文巧.jpeg';
