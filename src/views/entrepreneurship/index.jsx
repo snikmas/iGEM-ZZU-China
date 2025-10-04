bbd1fd863689dea8ac70830d18962f3100d76dab
Chandler Wang <wangpengjie218@gmail.com>
2025-10-04 16:05:07 +0000

    Update file index.jsx

--- src/views/entrepreneurship/index.jsx
+++ src/views/entrepreneurship/index.jsx
@@ -10,7 +10,7 @@ const descriptionFigure2 = 'https://static.igem.wiki/teams/5822/newassets/entrep
 const descriptionFigure3 = 'https://static.igem.wiki/teams/5822/newassets/entrepreneurship/entrepreneurshipfigure3.webp';
 const descriptionFigure4 = 'https://static.igem.wiki/teams/5822/newassets/entrepreneurship/entrepreneurshipfigure4.webp';
 
-const developmentPlanPdf = 'https://static.igem.wiki/teams/5822/newassets/images/entrepreneurship/developmentPlan.pdf';
+const developmentPlanPdf = 'https://static.igem.wiki/teams/5822/newassets/entrepreneurship/deve.webp';
 
 // 组件定义
 const MainContent = () => (
