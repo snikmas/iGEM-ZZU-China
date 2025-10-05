f9e36b98b891e24994474fab25ca387d522e8c69
li2842535779@stu.zzu.edu.cn <Lipei123#>
2025-10-05 12:30:03 +0800

    Home页面修改

--- src/views/homelanding/index.jsx
+++ src/views/homelanding/index.jsx
@@ -343,47 +343,47 @@ function HomeLanding() {
 
         {/* 滚动合作伙伴/赞助商 */}
         <div className="partners-scroll">
-        <div className="partners-track">
-    {/* 所有图标必须完全复制两次 */}
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/cdtm.webp" alt="Partner 1" className="partner-logo" />
-    </div>
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/hnscxcyjyjd.webp" alt="Partner 2" className="partner-logo" />
-    </div>
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/jjyxy.webp" alt="Partner 3" className="partner-logo" />
-    </div>
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdx.webp" alt="Partner 4" className="partner-logo" />
-    </div>
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdxdw.webp" alt="Partner 5" className="partner-logo" />
-    </div>
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdxdy.webp" alt="Partner 6" className="partner-logo" />
-    </div>
-    
-    {/* 完全相同的第二组 */}
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/cdtm.webp" alt="Partner 1" className="partner-logo" />
-    </div>
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/hnscxcyjyjd.webp" alt="Partner 2" className="partner-logo" />
-    </div>
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/jjyxy.webp" alt="Partner 3" className="partner-logo" />
-    </div>
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdx.webp" alt="Partner 4" className="partner-logo" />
-    </div>
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdxdw.webp" alt="Partner 5" className="partner-logo" />
-    </div>
-    <div className="partner-item">
-      <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdxdy.webp" alt="Partner 6" className="partner-logo" />
-    </div>
-  </div>
+          <div className="partners-track">
+            {/* 所有图标必须完全复制两次 */}
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/cdtm.webp" alt="Partner 1" className="partner-logo" />
+            </div>
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/hnscxcyjyjd.webp" alt="Partner 2" className="partner-logo" />
+            </div>
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/jjyxy.webp" alt="Partner 3" className="partner-logo" />
+            </div>
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdx.webp" alt="Partner 4" className="partner-logo" />
+            </div>
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdxdw.webp" alt="Partner 5" className="partner-logo" />
+            </div>
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdxdy.webp" alt="Partner 6" className="partner-logo" />
+            </div>
+
+            {/* 完全相同的第二组 */}
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/cdtm.webp" alt="Partner 1" className="partner-logo" />
+            </div>
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/hnscxcyjyjd.webp" alt="Partner 2" className="partner-logo" />
+            </div>
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/jjyxy.webp" alt="Partner 3" className="partner-logo" />
+            </div>
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdx.webp" alt="Partner 4" className="partner-logo" />
+            </div>
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdxdw.webp" alt="Partner 5" className="partner-logo" />
+            </div>
+            <div className="partner-item">
+              <img src="https://static.igem.wiki/teams/5822/newassets/home/zzdxdy.webp" alt="Partner 6" className="partner-logo" />
+            </div>
+          </div>
 
           <div className="contact-row">
             <div className="address-info">
@@ -404,47 +404,23 @@ function HomeLanding() {
           <div className="contact-row">
             <div className="address-info">
               <span className="icon">💻</span>
-              <a href="https://gitlab.igem.org/2025/zzu-china"  style={{color: '#333333'}}>
+              <a href="https://gitlab.igem.org/2025/zzu-china" style={{ color: '#333333' }}>
                 The repository used to create this website is available at gitlab.igem.org/2025/zzu-china.
               </a>
             </div>
           </div>
-          
 
-
-        </div>
-
-      </section>
-
-      {/* Section 12 - Footer */}
-      <section className="hl-section footer-hero">
-        <div className='footer-hero-pic'></div>
-        <div className='footer-hero-logo'>
-          <img src={logo} alt="HerScan Logo" className='footer-hero-logo-img' />
-        </div>
-
-        {/* 联系信息 */}
-        <div className="contact-info">
-          <h3>Contact Us</h3>
-          <p className="contact-text">Contact us on WeChat or YouTube</p>
-          <div className="address-info">
-            <p>Address: No. 100 Science Avenue, Zhengzhou City, Henan Province</p>
-            <p>Postcode: 450001</p>
+          <div className="socials" aria-label="Social links">
+            <a className="social-link wechat" href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzk3NTEzNjU4MQ==" target="_blank" rel="noreferrer" aria-label="WeChat" title="WeChat">
+              <WechatOutlined />
+            </a>
+            <a className="social-link youtube" href="https://youtube.com/@igemofzzu?si=zfx5budT1HFtjndj" target="_blank" rel="noreferrer" aria-label="YouTube" title="YouTube">
+              <YoutubeOutlined />
+            </a>
           </div>
-        </div>
 
-
-        <div className="socials" aria-label="Social links">
-          <a className="social-link wechat" href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzk3NTEzNjU4MQ==" target="_blank" rel="noreferrer" aria-label="WeChat" title="WeChat">
-            {/* <img src='https://static.igem.wiki/teams/5822/newassets/images/home/wechat.webp' alt="WeChat" className='social-icon' /> */}
-            <WechatOutlined />
-          </a>
-          <a className="social-link youtube" href="https://youtube.com/@igemofzzu?si=zfx5budT1HFtjndj" target="_blank" rel="noreferrer" aria-label="YouTube" title="YouTube">
-            <YoutubeOutlined />
-          </a>
         </div>
       </section>
-
     </div>
   );
 }
